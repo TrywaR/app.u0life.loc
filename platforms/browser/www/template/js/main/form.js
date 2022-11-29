3a3e2fa97069c450d186e0def59c0c0f60066074
TrywaR <TrywaR@yandex.ru>
2022-11-30 01:28:24 +0300

    5.6.0

--- platforms/browser/www/template/js/main/form.js
+++ platforms/browser/www/template/js/main/form.js
@@ -4,4 +4,10 @@ function forms_init( oForm ){
   $( oForm ).find('.switch select').each(function(){
     oForm.find('.switch_' + $(this).attr('name') + '-' + $(this).val()).addClass('_show_')
   })
+
+  // Чекбоксы
+  $( oForm ).find('input[type=checkbox]').on('change', function() {
+    if ( $(this).is(':checked') ) $(this).attr('value', 'true')
+    else $(this).attr('value', 'false')
+  })
 }
--- platforms/browser/www/template/js/main/main.js
+++ platforms/browser/www/template/js/main/main.js
@@ -1,5 +1,5 @@
 // Работа приложения
-sVersion = '5.5.51'
+sVersion = '5.6.0'
 // sSiteUrl = 'https://fttm.trywar.ru'
 sSiteUrl = 'https://u0life.com'
 // Параметры
